8790269c339a40d8a166fd3884c2d729c4b44b3c
oumaima ait baha <oumaima.aitbaha@edu.uiz.ac.ma>
2023-06-05 22:12:13 +0100

    KAFKA-SPRING-CLOUD-STREAMS

---
+++ src/main/java/com/example/springloudstreamskafka/SpringloudStreamsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springloudstreamskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringloudStreamsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringloudStreamsKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springloudstreamskafka/SpringloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
